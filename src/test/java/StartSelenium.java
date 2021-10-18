4732fe60ad83c2a306b89c8c666b2b5a2308e160
IrinaLerner <irina.bliudo14@gmail.com>
2021-10-18 19:40:56 +0300

    commit table test

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -15,6 +15,7 @@ public class StartSelenium {
     @BeforeMethod
     public void preCondition(){
         wd=new ChromeDriver();
+       // System.setProperty("webdriver.chrome.driver",);
        // wd.get("https://contacts-app.tobbymarshall815.vercel.app/login");
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/login");
        // wd.navigate().forward();
@@ -29,40 +30,55 @@ public class StartSelenium {
         List<WebElement> elements = wd.findElements(By.tagName("a"));
         System.out.println(elements.size());
 
-        wd.findElement(By.className("container"));
+
         wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        wd.findElement(By.xpath("//*[@class='navbar-component_nav__1X_4m']"));
         wd.findElement(By.className("active"));
         wd.findElement(By.className("login_login__3EHKB"));
 
+
         wd.findElement(By.id("root"));
         wd.findElement(By.cssSelector("#root"));
+        wd.findElement(By.xpath("//*[@id='root']"));
 
-        wd.findElement(By.tagName("div"));
+
         wd.findElement(By.tagName("h1"));
-        wd.findElement(By.tagName("a"));
+
         wd.findElement(By.tagName("input"));
         wd.findElement(By.tagName("br"));
         wd.findElement(By.tagName("button"));
         wd.findElement(By.tagName("script"));
 
+        wd.findElement(By.tagName("a"));
         wd.findElement(By.cssSelector("a"));
+        wd.findElement(By.xpath("//a"));
+        wd.findElement(By.tagName("div"));
         wd.findElement(By.cssSelector("div"));
+        wd.findElement(By.xpath("//div"));
+
+        wd.findElement(By.className("container"));
         wd.findElement(By.cssSelector(".container"));
+        wd.findElement(By.xpath("//*[@class='container']"));
 
 
-        wd.findElement(By.linkText("home"));
+        wd.findElement(By.linkText("HOME"));
         wd.findElement(By.linkText("about"));
         wd.findElement(By.linkText("login"));
-
+        wd.findElement(By.xpath("//*[@href='/login']"));
+        wd.findElement(By.cssSelector("[href]"));
         wd.findElement(By.partialLinkText("ho"));
         wd.findElement(By.partialLinkText("abo"));
         wd.findElement(By.partialLinkText("log"));
 
         wd.findElement(By.cssSelector("[href^='/login']"));//start
+        wd.findElement(By.xpath("//*[starts-with(@href,'/lo')]"));
         wd.findElement(By.cssSelector("[href$='/lin']"));//end
         wd.findElement(By.cssSelector("[href*='/ogi']"));//contains
+        wd.findElement(By.xpath("//*[contains(@href,'/lo')]"));
         wd.findElement(By.cssSelector("[href='/lo']"));
         wd.findElement(By.cssSelector("[href]"));
+
+
 
 
 
@@ -71,6 +87,9 @@ public class StartSelenium {
     }
     @Test
     public void testName2(){
+        wd.findElement(By.tagName("script"));
+        List<WebElement> s = wd.findElements(By.tagName("script"));
+        System.out.println("script : = " + s.size());
 
     }
 
---
+++ src/test/java/TableTest.java
@@ -0,0 +1,103 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class TableTest {
+
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void init() {
+        wd = new ChromeDriver();
+        wd.navigate().to("https://www.w3schools.com/css/css_table.asp");
+    }
+
+    @Test
+    public void tableTestCss() {
+        WebElement canada = wd.findElement(By.cssSelector("tr:nth-child(8) td:last-child"));
+        String text = canada.getText();
+
+        System.out.println(text);
+
+        Assert.assertEquals(text, "Canada");
+        Assert.assertTrue(text.contains("Can"));
+
+    }
+
+    @Test
+    public void tableHomeWork() {
+        //get row 4
+        WebElement el = wd.findElement(By.cssSelector("tr:nth-child(4)"));
+        String text =el.getText();
+        System.out.println(text);
+        Assert.assertEquals(text,"Centro comercial Moctezuma Francisco Chang Mexico");
+        Assert.assertTrue(text.contains("Francisco Chang"));
+
+        String text2 = wd.findElement(By.cssSelector("tr:nth-child(4) td:nth-child(2)")).getText();
+        Assert.assertEquals(text2,"Francisco Chang");
+        Assert.assertEquals(wd.findElement(By.cssSelector("tr:nth-child(4) td:nth-child(2)")).getText(),"Francisco Chang");
+
+        //Francisco Chang
+        // Assert Francisco Chang?
+    }
+
+    @Test
+    public void tableHW2() {
+        //print numbers of rows
+
+        List<WebElement> rows = wd.findElements(By.cssSelector("tr"));
+        System.out.println(rows.size());
+        Assert.assertEquals(rows.size(),9);
+
+        //print numbers of columns
+        List<WebElement> columns = wd.findElements(By.cssSelector("tr th"));
+        System.out.println("Print numbers of last columns -->"+ columns.size());
+        System.out.println("*************************");
+
+        //print rows 3
+        List<WebElement> row3=wd.findElements(By.cssSelector("tr:nth-child(3) td"));
+        System.out.println("Row 3 --->" +row3.size() );
+
+        for (WebElement el :row3) {
+            System.out.println(el.getText());
+        }
+        for (int i = 0; i < row3.size(); i++) {
+            System.out.println(rows.get(i).getText());
+        }
+
+        //print lastColumns
+
+        List<WebElement> lastColumns= wd.findElements(By.cssSelector("td:last-child"));
+        for (WebElement el:
+                lastColumns) {
+            System.out.println(el.getText());
+        }
+
+        //print Maria Anders
+        System.out.println(wd.findElement(By.cssSelector("tr:nth-child(2) td:nth-child(2)")).getText());
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+
+    @Test
+    public void tableHw2() {
+
+    }
+
+}
+
+
+
+
+
